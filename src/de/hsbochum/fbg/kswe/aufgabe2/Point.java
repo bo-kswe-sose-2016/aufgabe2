b13829402abb06826326141ec2d26d22060bfa37
matthesrieke <m.rieke@52north.org>
2016-04-03 15:29:49 +0200

    add simple classes

---
+++ src/de/hsbochum/fbg/kswe/aufgabe2/Point.java
@@ -0,0 +1,22 @@
+package de.hsbochum.fbg.kswe.aufgabe2;
+
+/**
+ *
+ * @author Matthes Rieke <m.rieke@52north.org>
+ */
+public class Point {
+    
+    private final double latitude;
+    private final double longitude;
+
+    public Point(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public double distanceTo(Point other) {
+        //TODO implement
+        return 0.0;
+    }
+    
+}
