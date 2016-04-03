0f092d22749363d0758cd4cb373323f6d5c3b90f
matthesrieke <m.rieke@52north.org>
2016-04-03 15:59:25 +0200

    implement distance using sphere calculations

--- src/de/hsbochum/fbg/kswe/aufgabe2/Point.java
+++ src/de/hsbochum/fbg/kswe/aufgabe2/Point.java
@@ -8,15 +8,24 @@ public class Point {
     
     private final double latitude;
     private final double longitude;
-
+    
     public Point(double latitude, double longitude) {
         this.latitude = latitude;
         this.longitude = longitude;
     }
-
+    
     public double distanceTo(Point other) {
-        //TODO implement
-        return 0.0;
+        double radius = 6371000;
+        
+        double latDelta = Math.toRadians(Math.abs(this.latitude - other.latitude));
+        double lonDelta = Math.toRadians(Math.abs(this.longitude - other.longitude));
+        double a = Math.sin(latDelta/2) * Math.sin(latDelta/2) +
+                Math.cos(Math.toRadians(this.latitude)) * Math.cos(Math.toRadians(other.latitude)) *
+                Math.sin(lonDelta/2) * Math.sin(lonDelta/2);
+        
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        
+        return (double) (radius * c);
     }
     
 }
